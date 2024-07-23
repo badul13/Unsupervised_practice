ebfa2d8d6af2bd2a7841b72351a47bbfc4499eaf
badul13 <144805932+badul13@users.noreply.github.com>
2024-07-23 15:35:02 +0900

    Add files via upload

--- AE_number.py
+++ AE_number.py
@@ -1,5 +1,5 @@
 from keras import Input
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Flatten, Dropout, BatchNormalization, Reshape, LeakyReLU
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Flatten, Dropout, BatchNormalization, Reshape, LeakyReLU, Add
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint
 import tensorflow as tf
@@ -18,69 +18,68 @@ if gpus:
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_valid, y_valid) = mnist.load_data()
-x_train.shape, y_train.shape
 x_train = x_train.reshape(-1, 28, 28, 1)
 x_train = x_train / 127.5 - 1
-x_train.min(), x_train.max()
 
 encoder_input = Input(shape=(28, 28, 1))
 
-# 28 X 28
+# Encoder with residual connections
+def residual_block(x, filters, kernel_size=3):
+    shortcut = x
+    x = Conv2D(filters, kernel_size, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(filters, kernel_size, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Add()([x, shortcut])
+    x = LeakyReLU()(x)
+    return x
+
 x = Conv2D(32, 3, padding='same')(encoder_input)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
+x = residual_block(x, 32)
 
-# 28 X 28 -> 14 X 14
 x = Conv2D(64, 3, strides=2, padding='same')(x)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
+x = residual_block(x, 64)
 
-# 14 X 14 -> 7 X 7
 x = Conv2D(64, 3, strides=2, padding='same')(x)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
-
-# 17 X 7
-x = Conv2D(64, 3, padding='same')(x)
-x = BatchNormalization()(x)
-x = LeakyReLU()(x)
+x = residual_block(x, 64)
 
 x = Flatten()(x)
-
-# 2D 좌표로 표기하기 위하여 2를 출력값으로 지정합니다.
 encoder_output = Dense(2)(x)
 
 encoder = Model(encoder_input, encoder_output)
 encoder.summary()
 
-# Input으로는 2D 좌표가 들어갑니다.
-decoder_input = Input(shape=(2, ))
-
-# 2D 좌표를 7*7*64 개의 neuron 출력 값을 가지도록 변경합니다.
+decoder_input = Input(shape=(2,))
 x = Dense(7*7*64)(decoder_input)
-x = Reshape( (7, 7, 64))(x)
+x = Reshape((7, 7, 64))(x)
 
-# 7 X 7 -> 7 X 7
 x = Conv2DTranspose(64, 3, strides=1, padding='same')(x)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
+x = residual_block(x, 64)
 
-# 7 X 7 -> 14 X 14
 x = Conv2DTranspose(64, 3, strides=2, padding='same')(x)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
+x = residual_block(x, 64)
 
-# 14 X 14 -> 28 X 28
 x = Conv2DTranspose(64, 3, strides=2, padding='same')(x)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
+x = residual_block(x, 64)
 
-# 28 X 28 -> 28 X 28
 x = Conv2DTranspose(32, 3, strides=1, padding='same')(x)
 x = BatchNormalization()(x)
 x = LeakyReLU()(x)
+x = residual_block(x, 32)
 
-# 최종 output
 decoder_output = Conv2DTranspose(1, 3, strides=1, padding='same', activation='tanh')(x)
 
 decoder = Model(decoder_input, decoder_output)
@@ -94,31 +93,19 @@ x = encoder(encoder_in)
 decoder_out = decoder(x)
 
 auto_encoder = Model(encoder_in, decoder_out)
-
 auto_encoder.compile(optimizer=tf.keras.optimizers.Adam(LEARNING_RATE), loss=tf.keras.losses.MeanSquaredError())
 
 checkpoint_path = 'imcheckpoint.weights.h5'
-checkpoint = ModelCheckpoint(checkpoint_path,
-                             save_best_only=True,
-                             save_weights_only=True,
-                             monitor='loss',
-                             verbose=1)
+checkpoint = ModelCheckpoint(checkpoint_path, save_best_only=True, save_weights_only=True, monitor='loss', verbose=1)
 
-
-auto_encoder.fit(x_train, x_train,
-                 batch_size=BATCH_SIZE,
-                 epochs=40,
-                 callbacks=[checkpoint],
-                )
+auto_encoder.fit(x_train, x_train, batch_size=BATCH_SIZE, epochs=40, callbacks=[checkpoint])
 
 auto_encoder.load_weights(checkpoint_path)
-
 
 import matplotlib.pyplot as plt
 
 # MNIST 이미지에 대하여 x, y 좌표로 뽑아냅니다.
 xy = encoder.predict(x_train)
-xy.shape, y_train.shape
 
 plt.figure(figsize=(15, 12))
 plt.scatter(x=xy[:, 0], y=xy[:, 1], c=y_train, cmap=plt.get_cmap('Paired'), s=3)
